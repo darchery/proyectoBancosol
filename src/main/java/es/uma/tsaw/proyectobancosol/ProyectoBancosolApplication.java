2044a627fc0762f3b580bfe2577ab516c91d1d98
Lucas Díaz Ruiz <anestardiaz@gmail.com>
2026-04-13 13:35:32 +0200

    proyecto intelij vacío
    Add src, test , pom y gitignore

---
+++ src/main/java/es/uma/tsaw/proyectobancosol/ProyectoBancosolApplication.java
@@ -0,0 +1,13 @@
+package es.uma.tsaw.proyectobancosol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBancosolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoBancosolApplication.class, args);
+    }
+
+}
---
+++ test/java/es/bancosol/campanyas/CampanyasApplicationTests.java
@@ -0,0 +1,13 @@
+package es.bancosol.campanyas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampanyasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
