a8e13dd56eb6caa5a86a5deb83394996d338e1eb
darchery <anestardiaz@gmail.com>
2026-04-20 13:21:19 +0200

    Add BancosolController y mv de prueba.jsp
    Controller de prueba para probar la conexión a la db

---
+++ src/main/java/es/uma/tsaw/proyectobancosol/controller/BancosolController.java
@@ -0,0 +1,13 @@
+package es.uma.tsaw.proyectobancosol.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BancosolController {
+
+    @GetMapping("/prueba")
+    public String prueba() {
+        return "prueba";
+    }
+}
