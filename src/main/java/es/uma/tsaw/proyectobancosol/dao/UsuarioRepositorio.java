3bf0bac0c2bf9bc3e8e010c336216cb375bdc2af
laiadiaz <148168212+laiadiaz@users.noreply.github.com>
2026-04-24 10:44:57 +0200

    modificaciones controlador

--- src/main/java/es/uma/tsaw/proyectobancosol/dao/UsuarioRepositorio.java
+++ src/main/java/es/uma/tsaw/proyectobancosol/dao/UsuarioRepositorio.java
@@ -1,6 +1,11 @@
-// UsuarioRepositorio.java  ← UUID, no Integer!
 package es.uma.tsaw.proyectobancosol.dao;
+
 import es.uma.tsaw.proyectobancosol.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 import java.util.UUID;
-public interface UsuarioRepositorio extends JpaRepository<Usuario, UUID> {}
+
+public interface UsuarioRepositorio extends JpaRepository<Usuario, UUID> {
+    List<Usuario> findByRolNombreRol(String voluntario);
+}
