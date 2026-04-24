ef1d04d8fb7c3012b0615d5efdd296ffef904333
Daniela <daniela.calderon@uma.es>
2026-04-24 10:06:21 +0200

    cambios

---
+++ src/main/java/es/uma/tsaw/proyectobancosol/dao/TurnoActivoRepositorio.java
@@ -0,0 +1,5 @@
+// TurnoRepositorio.java
+package es.uma.tsaw.proyectobancosol.dao;
+import es.uma.tsaw.proyectobancosol.entity.TurnoActivo;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface TurnoActivoRepositorio extends JpaRepository<TurnoActivo, Integer> {}
