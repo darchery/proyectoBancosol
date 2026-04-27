b607a91bed2cbd27493a0386d997871989836c6e
BlinckyP <mariinaruiizuni@gmail.com>
2026-04-27 11:06:51 +0200

    añado env :)

--- src/main/java/es/uma/tsaw/proyectobancosol/controller/AsignacionController.java
+++ src/main/java/es/uma/tsaw/proyectobancosol/controller/AsignacionController.java
@@ -1,11 +1,26 @@
 package es.uma.tsaw.proyectobancosol.controller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
+import java.util.List;
+import es.uma.tsaw.proyectobancosol.dao.AsignacionVoluntarioRepositorio;
+import es.uma.tsaw.proyectobancosol.entity.AsignacionVoluntario;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@AllArgsConstructor
 @RequestMapping("/asignacion")
 public class AsignacionController {
 
+    private final AsignacionVoluntarioRepositorio asignacionRepositorio;
+
+    @GetMapping("/voluntarios")
+    public String listarAsignaciones(Model model) {
+        List<AsignacionVoluntario> asignaciones = asignacionRepositorio.findAll();
+        model.addAttribute("asignaciones", asignaciones);
+        return "gestionVoluntarios";
+    }
 
 }
--- src/main/java/es/uma/tsaw/proyectobancosol/dao/AsignacionVoluntarioRepositorio.java
+++ src/main/java/es/uma/tsaw/proyectobancosol/dao/AsignacionVoluntarioRepositorio.java
@@ -2,4 +2,10 @@
 package es.uma.tsaw.proyectobancosol.dao;
 import es.uma.tsaw.proyectobancosol.entity.AsignacionVoluntario;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface AsignacionVoluntarioRepositorio extends JpaRepository<AsignacionVoluntario, Integer> {}
+
+import java.util.List;
+
+public interface AsignacionVoluntarioRepositorio extends JpaRepository<AsignacionVoluntario, Integer> {
+
+    List<AsignacionVoluntario> findByTurnoActivoIdTurnoActivo(Integer idTurnoActivo);
+}
